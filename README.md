395b3da78326a187e52c8db69d744f884569f6e5
chocobo <e1b17065@st.oit.ac.jp>
2018-05-24 13:56:09 +0900

    細かいところを修正

--- README.md
+++ README.md
@@ -1,13 +1,47 @@
 # TweetBot_in_Slack
 
-## mecab-ipadic-NEologdのインストール
+ツイートの文章から新しい語録を生み出すbotです。
+
+## 大まかな使い方
+
+- python3をインストール
+
+- mecab-ipadic-NEologdのインストール
 ```
 $ git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
 $ cd mecab-ipadic-neologd
 $ ./bin/install-mecab-ipadic-neologd -n
 ```
 
-## mecab-python3のインストール
+- mecab-python3のインストール
 ```
 $ pip3 install mecab-python3
+```
+
+- TweetBot_in_Slackのダウンロード
+```
+$ https://github.com/chocobo777/TweetBot_in_Slack.git
+$ cd TweetBot_in_Slack
+```
+
+- ```.env``` ファイルを作成
+```
+$ vi .env
+
+SLACK_API=##################################################
+```
+
+- twitterから過去のツイートを取得し、tweets.csvをvariousフォルダに置く
+
+- 辞書を生成
+```
+$ cd verious
+$ python3 db.py
+$ python3 conversion.py
+```
+
+
+- botの起動
+```
+$ python3 run.py
 ```
--- plugins/bot.py
+++ plugins/bot.py
@@ -3,14 +3,10 @@
 from slackbot.bot import respond_to
 import random
 
-@respond_to("もちもち")
-def motimoti(message):
-    message.reply("モチモチモチモチモﾁﾓﾁﾓﾁﾓ(ﾉ)`ω´(ヾ)(ﾉ)・ω・(ゞ) もちもちモチモチﾓﾁﾓﾁﾓ(ﾉ)`ω´(ヾ)(ﾉ)`ω´(ヾ)ﾓﾁｯモチモチモチモチモﾁﾓﾁﾓﾁﾓ(ﾉ)`ω´(ヾ)(ﾉ)・ω・(ゞ) もちもちモチモチモチモチモﾁﾓﾁﾓﾁﾓ(ﾉ)`ω´(ヾ)(ﾉ)'∨'(ヾ)ﾓﾁﾓﾁ(ﾉ)･∀･(ヾ)ﾓﾁﾓﾁモチモチﾓﾁﾓﾁﾓ(ﾉ)`ω")
-
 @respond_to("eliza")
 def eliza(message):
     # ファイルを開く
-    file = open("words.txt", "r")
+    file = open("words.txt", "r") # words.txtはrun.pyと同じディレクトリに置かないといけないっぽい？
 
     words = file.read()
     wordlist = words.split("\n")
@@ -23,3 +19,11 @@ def eliza(message):
     message.reply(text)
 
     file.close()
+
+@respond_to("もち")
+def moti(message):
+    message.reply("(ﾉ)`ω´(ヾ)")
+
+@respond_to("もちもち")
+def motimoti(message):
+    message.reply("モチモチモチモチモﾁﾓﾁﾓﾁﾓ(ﾉ)`ω´(ヾ)(ﾉ)・ω・(ゞ) もちもちモチモチﾓﾁﾓﾁﾓ(ﾉ)`ω´(ヾ)(ﾉ)`ω´(ヾ)ﾓﾁｯモチモチモチモチモﾁﾓﾁﾓﾁﾓ(ﾉ)`ω´(ヾ)(ﾉ)・ω・(ゞ) もちもちモチモチモチモチモﾁﾓﾁﾓﾁﾓ(ﾉ)`ω´(ヾ)(ﾉ)'∨'(ヾ)ﾓﾁﾓﾁ(ﾉ)･∀･(ヾ)ﾓﾁﾓﾁモチモチﾓﾁﾓﾁﾓ(ﾉ)`ω")
